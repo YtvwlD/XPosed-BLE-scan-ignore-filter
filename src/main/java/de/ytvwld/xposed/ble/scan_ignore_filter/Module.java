688bf0199538cc74643638b73ede7a6cc3ba5db9
Niklas Sombert <niklas@ytvwld.de>
2017-03-31 12:06:15 +0200

    Add logging.

--- src/main/java/de/ytvwld/xposed/ble/scan_ignore_filter/Module.java
+++ src/main/java/de/ytvwld/xposed/ble/scan_ignore_filter/Module.java
@@ -16,6 +16,7 @@ public class Module implements IXposedHookZygoteInit
             @Override
             protected void beforeHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable
             {
+                XposedBridge.log("startLeScan: Ignoring filter: " + param.args[0]);
                 param.args[0] = null;
             }
         });
