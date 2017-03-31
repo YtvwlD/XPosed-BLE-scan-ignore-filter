88ee7d64da2adb3a1319b58327e05fcc38112eb0
Niklas Sombert <niklas@ytvwld.de>
2017-03-31 12:39:49 +0200

    Improve logging.

--- src/main/java/de/ytvwld/xposed/ble/scan_ignore_filter/Module.java
+++ src/main/java/de/ytvwld/xposed/ble/scan_ignore_filter/Module.java
@@ -6,6 +6,7 @@ import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 
 import java.util.UUID;
+import java.util.Arrays;
 import android.bluetooth.BluetoothAdapter.LeScanCallback;
 
 public class Module implements IXposedHookZygoteInit
@@ -16,8 +17,11 @@ public class Module implements IXposedHookZygoteInit
             @Override
             protected void beforeHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable
             {
-                XposedBridge.log("startLeScan: Ignoring filter: " + param.args[0]);
-                param.args[0] = null;
+                if(param.args[0] != null)
+                {
+                    XposedBridge.log("startLeScan: Ignoring filter: " + Arrays.deepToString(param.args[0]));
+                    param.args[0] = null;
+                }
             }
         });
     }
