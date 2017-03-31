b1e6f5e1837620fafc656bb5342306135152abce
Niklas Sombert <niklas@ytvwld.de>
2017-03-31 11:48:08 +0200

    Add core part.

---
+++ src/main/java/de/ytvwld/xposed/ble/scan_ignore_filter/Module.java
@@ -0,0 +1,23 @@
+package de.ytvwld.xposed.ble.scan_ignore_filter;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+import de.robv.android.xposed.IXposedHookZygoteInit;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+
+import java.util.UUID;
+import android.bluetooth.BluetoothAdapter.LeScanCallback;
+
+public class Module implements IXposedHookZygoteInit
+{
+    public void initZygote(final IXposedHookZygoteInit.StartupParam startupParam) throws Throwable
+    {
+        findAndHookMethod("android.bluetooth.BluetoothAdapter", null, "startLeScan", UUID.class, LeScanCallback.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable
+            {
+                param.args[0] = null;
+            }
+        });
+    }
+}
